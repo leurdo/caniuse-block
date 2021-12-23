dcc7d46539f20ae2b7a6c27effea811add380ca1
Mikhail Kobzarev <mihdan@yandex.ru>
2021-12-24 00:43:36 +0300

    Add ability for updating plugin via GitHub Updater

--- caniuse-block.php
+++ caniuse-block.php
@@ -9,6 +9,7 @@
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       caniuse-block
+ * GitHub Plugin URI: https://github.com/leurdo/caniuse-block/
  *
  * @package           caniuse-block
  */
